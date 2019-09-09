b9ac9cf76436d7e79f3049089125ecdb1ec83f58
Phil Whittaker <phil@hifi.agency>
2019-09-09 06:39:15 +0100

    version

--- YuzuDelivery.Core/Properties/AssemblyInfo.cs
+++ YuzuDelivery.Core/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Yuzu.Delivery.Blocks2")]
+[assembly: AssemblyTitle("YuzuDelivery.Core")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Yuzu.Delivery.Blocks2")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyProduct("YuzuDelivery.Core")]
+[assembly: AssemblyCopyright("Copyright ©HiFi Ltd 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
