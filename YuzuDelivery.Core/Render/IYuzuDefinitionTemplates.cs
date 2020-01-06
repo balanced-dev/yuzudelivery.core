0de34e52997d6118e77b0c8d11833a70ae7ba266
Phil Whittaker <phil@hifi.agency>
2020-01-06 19:59:50 +0000

    update appveryor build and license details

--- YuzuDelivery.Core/Render/IYuzuDefinitionTemplates.cs
+++ YuzuDelivery.Core/Render/IYuzuDefinitionTemplates.cs
@@ -4,10 +4,7 @@ namespace YuzuDelivery.Core
 {
     public interface IYuzuDefinitionTemplates
     {
-        string AddCurrentJsonToTemplate(IRenderSettings settings, object data, string html);
-        object CreateData(IRenderSettings settings);
         string Render(IRenderSettings settings);
         string Render<E>(IRenderSettings settings, IDictionary<string, object> mappingItems);
-        string RenderTemplate(IRenderSettings settings, object data);
     }
 }
