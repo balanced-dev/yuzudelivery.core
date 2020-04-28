c1fe8923bec38fceef4b6a9477a2f1743f66f2bb
Phil Whittaker <phil@hifi.agency>
2020-04-28 07:40:41 +0100

    minor fix

--- YuzuDelivery.Core/Mapper/ResolverInterfaces/IYuzuPropertyAfterResolver.cs
+++ YuzuDelivery.Core/Mapper/ResolverInterfaces/IYuzuPropertyAfterResolver.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq.Expressions;
 
-namespace YuzuDelivery.Umbraco.Core
+namespace YuzuDelivery.Core
 {
     public interface IYuzuPropertyAfterResolver { }
 }
