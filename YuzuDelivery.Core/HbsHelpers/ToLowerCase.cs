e26cd4ab24075f4e146de77385d1045a3139766a
Phil Whittaker <phil@hifi.agency>
2020-09-17 16:24:58 +0100

    - add to lower case hbs helper

---
+++ YuzuDelivery.Core/HbsHelpers/ToLowerCase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HandlebarsDotNet;
+
+namespace YuzuDelivery.Core
+{
+    public class ToLowerCase
+    {
+        public ToLowerCase()
+        {
+            HandlebarsDotNet.Handlebars.RegisterHelper("toLowerCase", (writer, context, parameters) =>
+            {
+                writer.WriteSafeString(parameters[0].ToString().ToLower());
+            });
+        }
+    }
+}
