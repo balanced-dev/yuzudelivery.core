7dd863f10fc02c2adbfb192946594d6ffa53fddd
Phil Whittaker <phil@hifi.agency>
2021-10-06 21:09:36 +0100

    add app settings

---
+++ YuzuDelivery.Core/AppSettings/CoreSettings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace YuzuDelivery.Umbraco.Core
+{
+    public class CoreSettings
+    {
+        internal const string StaticPages = "/yuzu/_templates/src/pages";
+        internal const string StaticPartials = "/yuzu/_templates/src/blocks";
+        internal const string StaticSchemaMeta = "/yuzu/_templates/paths";
+
+        [DefaultValue(StaticPages)]
+        public string Pages { get; set; } = StaticPages;
+
+        [DefaultValue(StaticPartials)]
+        public string Partials { get; set; } = StaticPartials;
+
+        [DefaultValue(StaticSchemaMeta)]
+        public string SchemaMeta { get; set; } = StaticSchemaMeta;
+    }
+}
