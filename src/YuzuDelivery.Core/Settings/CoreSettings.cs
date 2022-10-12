2ff134fe32ea379fdd52d42bea92feda095959cf
Paul Johnson <paul@hifi.agency>
2022-10-12 14:57:37 +0100

    Drop path leading slashes

--- src/YuzuDelivery.Core/Settings/CoreSettings.cs
+++ src/YuzuDelivery.Core/Settings/CoreSettings.cs
@@ -4,10 +4,10 @@ namespace YuzuDelivery.Core.Settings
 {
     public class CoreSettings
     {
-        private const string StaticPages = "/Yuzu/_templates/src/pages";
-        private const string StaticPartials = "/Yuzu/_templates/src/blocks";
-        private const string StaticSchemaMeta = "/Yuzu/_templates/paths";
-        private const string StaticConfigPath = "/Yuzu//YuzuConfig.json";
+        private const string StaticPages = "Yuzu/_templates/src/pages";
+        private const string StaticPartials = "Yuzu/_templates/src/blocks";
+        private const string StaticSchemaMeta = "Yuzu/_templates/paths";
+        private const string StaticConfigPath = "Yuzu//YuzuConfig.json";
 
         [DefaultValue(StaticPages)]
         public string Pages { get; set; } = StaticPages;
