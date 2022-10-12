68938a7d4bc773b86648342ab9d3fc786e4c01e8
Paul Johnson <paul@hifi.agency>
2022-10-12 14:42:50 +0100

    Move CoreSettings to Settings namespace

--- src/YuzuDelivery.Core/Settings/CoreSettings.cs
+++ src/YuzuDelivery.Core/Settings/CoreSettings.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 
-namespace YuzuDelivery.Umbraco.Core
+namespace YuzuDelivery.Core.Settings
 {
     public class CoreSettings
     {
