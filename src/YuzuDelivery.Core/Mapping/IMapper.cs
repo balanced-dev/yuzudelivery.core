a33804bb44e4fd95a5e7815fc4447cf57202876c
Paul Johnson <paul@hifi.agency>
2023-01-05 16:59:23 +0000

    Convenience method on IMapper

--- src/YuzuDelivery.Core/Mapping/IMapper.cs
+++ src/YuzuDelivery.Core/Mapping/IMapper.cs
@@ -11,5 +11,10 @@ namespace YuzuDelivery.Core.Mapping
         TDest Map<TDest>(object source, IDictionary<string, object> items);
         TDest Map<TSource, TDest>(TSource source, IDictionary<string, object> items);
         object Map(object source, Type sourceType, Type destinationType, IDictionary<string, object> items);
+
+        IYuzuViewModel Map(object source, Type destinationType, IDictionary<string, object> items = null)
+        {
+            return Map(source, source.GetType(), destinationType, items ?? new Dictionary<string, object>()) as IYuzuViewModel;
+        }
     }
 }
